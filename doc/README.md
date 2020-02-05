20999de98a545c45d2455bf833b821f17a628971
Nicolas <nicolas.delhomme@umu.se>
2020-02-05 16:47:22 +0100

    Added the sample info for all projects Added the salmon script to submit all datasets Updated the common Git submodule Added a sample info README

---
+++ doc/README.md
@@ -0,0 +1,14 @@
+# SRA
+The SRA metadata was created using [Curse](http://bioinformatics.psb.ugent.be/webtools/Curse/index.php)
+
+# UPSC
+The 4 datasets are:
+
+1. Atlas: the genome project mRNA atlas
+2. Diurnal: Maria Eriksson and Marek Mutwil diurnal datasets (2 days at LD (long days) and for ME, an additional set at LL (Long Light))
+3. Flakaliden: Vaughan Hurry Flakaliden experiment site. N fertilisation for 0, 5, 25 years.
+4. Seasonal: Stefan Jansson seasonal data, samples collected all across the season
+
+All the datasets consists of needles only, apart from 1. that has other tissues. These will not be considered
+
+The datasets have been sequenced in a strand specific manner, in paired-end mode but for 1. and the MW diurnal set that were sequenced non-stranded. The latter was also sequenced as single-end only.
--- pipeline/submitSalmon.sh
+++ pipeline/submitSalmon.sh
@@ -3,32 +3,113 @@
 ## be verbose and print
 set -eux
 
-proj=u2019016
-mail=mist0069@student.umu.se
-
-## process the argument
+# params
+proj=facility
+mail=nicolas.delhomme@umu.se
 ref=/mnt/picea/storage/reference/Picea-abies/v1.0/indices/salmon/Pabies1.0-all-phase.gff3.CDSandLTR-TE
 bind=/mnt:/mnt
-img=/mnt/picea/projects/singularity/salmon-0.14.1.simg 
+img=/mnt/picea/projects/singularity/salmon-1.1.0.simg 
+in=/mnt/picea/projects/spruce/facility/trimmomatic
+out=$(realpath ../data/salmon)
 
-## Zygotic
-in=/mnt/picea/projects/spruce/uegertsdotter/ZE-developmental-series/RNA-Seq/trimmomatic
-out=/mnt/picea/projects/spruce/uegertsdotter/ZE-developmental-series/RNA-Seq/salmon
-
-## create the out dir
-if [ ! -d $out ]; then
-    mkdir -p $out
-fi
-
-## for every file
-
-for f in $(find $in -name "*_trimmomatic_1.fq.gz"); do
-  fnam=$(basename ${f/_1.fq.gz/})
+declare -A DATASET=(
+  [atlas]="atlas"
+  [diurnal]="diurnal"
+  [diurnal2]="diurnal"
+  [flakaliden]="flakaliden"
+  [seasonal]="seasonal")
   
-  ## execute
-sbatch -A $proj --mail-user=$mail \
-  -e $out/$fnam.err -o $out/$fnam.out -J salmon.$fnam \
-  ../UPSCb-common/pipeline/runSalmon.sh -b $bind \
-  -i $img $ref $f $in/${fnam}_2.fq.gz $out
+declare -A RUN=(
+  [atlas]=1
+  [diurnal]=1
+  [diurnal2]=1
+  [flakaliden]=1
+  [seasonal]=1
+  )
 
+default="*_trimmomatic_1.fq.gz"
+
+declare -A PATTERN=(
+  [atlas]=$default
+  [diurnal]=$default
+  [diurnal2]="*_trimmomatic.fq.gz"
+  [flakaliden]=$default
+  [seasonal]=$default
+)
+
+default="../UPSCb-common/pipeline/runSalmon.sh"
+
+declare -A TOOL=(
+  [atlas]=$default
+  [diurnal]=$default
+  [diurnal2]="../UPSCb-common/pipeline/runSalmonSE.sh ADD-EXTRA-ARGS-THERE fragment length mean and fragment length sd"
+  [flakaliden]=$default
+  [seasonal]=$default
+)
+
+default=1
+declare -A PE=(
+  [atlas]=$default
+  [diurnal]=$default
+  [diurnal2]=0
+  [flakaliden]=$default
+  [seasonal]=$default
+)
+
+# helper
+source ../UPSCb-common/src/bash/functions.sh
+
+# variables
+FORCE=0
+
+# usage
+USAGETXT=\
+"
+$0 [options]
+  
+  Options -f force the rebuild
+"
+
+while getopts f option
+do
+    case "$option" in
+    f) FORCE=1;;
+    \?) ## unknown flag
+	    usage;;
+  esac
 done
+shift `expr $OPTIND - 1`
+
+# Dataset
+for DSET in "${!DATASET[@]}"; do
+  if [ ${RUN[$DSET]} -eq 1 ]; then
+  
+    outDir=$out/${DATASET[$DSET]} 
+    
+    ## create the out dir
+    if [ ! -d $outDir ]; then
+      mkdir -p $outDir
+    fi
+  
+    ## for every file
+    REV=
+    for f in $(find $in/${DATASET[$DSET]} -name ${PATTERN[$DSET]}); do
+      if [ ${PE[$DSET]} -eq 1 ]; then
+        fnam=$(basename ${f/_1.fq.gz/})
+        REV=$in/$DSET/${fnam}_2.fq.gz
+      else
+        fnam=$(basename ${f/.fq.gz/})
+      fi
+      
+      ## execute
+      echo sbatch -A $proj --mail-user=$mail \
+        -e $outDir/$fnam.err -o $outDir/$fnam.out -J salmon.$fnam \
+        ${TOOL[$DSET]} -b $bind \
+        -i $img $ref $f $REV $outDir
+    done
+  fi
+done
+  
+
+
+
